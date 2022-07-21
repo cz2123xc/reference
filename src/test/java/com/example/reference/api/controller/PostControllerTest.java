8822d5832c9d49e302f7b24ec0d9b0880bd0eaf1
지금이야 <cz2123xc@gmail.com>
2022-07-22 01:58:44 +0900

    테스트 코드 오류 수정

--- src/test/java/com/example/reference/api/controller/PostControllerTest.java
+++ src/test/java/com/example/reference/api/controller/PostControllerTest.java
@@ -67,10 +67,42 @@ class PostControllerTest {
     @DisplayName("/post 요청시 title 값은 필수다.")
     void test2() throws Exception {
 
+        // given
+        PostCreate request = PostCreate.builder()
+                .content("내용입니다.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
         // when
         mockMvc.perform(post("/posts")
                         .contentType(APPLICATION_JSON)
-                        .content("{\"title\":\"제목입니다.\", \"content\":\"내용 입니다.\"}")
+                        .content(json)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청 입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/posts 요청시 DB에 값이 저장된다.")
+    void test3() throws Exception{
+
+        // given
+        PostCreate request = PostCreate.builder()
+                        .title("제목입니다.")
+                        .content("내용입니다.")
+                        .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+
+        // when
+        mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -81,9 +113,9 @@ class PostControllerTest {
         Post post = postRepository.findAll().get(0);
 
         assertEquals("제목입니다.", post.getTitle());
-        assertEquals("내용 입니다.", post.getContent());
-
+        assertEquals("내용입니다.", post.getContent());
     }
+
 
 
 
